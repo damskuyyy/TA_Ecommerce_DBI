75d12f0b78b0b833a5f46251e6013dcbff3e7b84
Nafiskurniatul <92704434+Nafiskurniatul@users.noreply.github.com>
2024-09-10 10:40:53 +0700

    update

--- src/pages/user/signup/index.tsx
+++ src/pages/user/signup/index.tsx
@@ -1,127 +1,144 @@
-import Link from "next/link"
-
-import { Button } from "@/components/ui/button"
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import Head from "next/head"
-import { signIn } from "next-auth/react"
-import { FormEvent, useEffect, useRef, useState } from "react"
-import axios from "axios"
-import { useToast } from "@/components/ui/use-toast"
-import { useRouter } from "next/router"
-import { cn } from "@/lib/utils"
-import { Shell } from "lucide-react"
-import Combobox from "@/components/ui/combobox"
-
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Head from "next/head";
+import { signIn } from "next-auth/react";
+import { FormEvent, useEffect, useRef, useState } from "react";
+import axios from "axios";
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/router";
+import { cn } from "@/lib/utils";
+import { Shell } from "lucide-react";
+import Combobox from "@/components/ui/combobox";
+import { Checkbox } from "@/components/ui/checkbox";
 
 export default function Signup() {
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
-  const [email, setEmail] = useState('')
-  const [phone, setPhone] = useState('')
-  const { push } = useRouter()
-  const { toast } = useToast()
-  const [load, setLoad] = useState(false)
-  const [dialCode, setDialCode] = useState('')
-  const phoneInputRef = useRef<HTMLInputElement>(null)
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [isTermsAgreed, setIsTermsAgreed] = useState(false);
+  const { push } = useRouter();
+  const { toast } = useToast();
+  const [load, setLoad] = useState(false);
+  const [dialCode, setDialCode] = useState("");
+  const phoneInputRef = useRef<HTMLInputElement>(null);
+
+  const handleCheckboxChange = () => {
+    setIsTermsAgreed(!isTermsAgreed);
+  };
 
   const handlesignupGoogle = async () => {
     try {
-      await signIn('google', { redirect: false, callbackUrl: '/' })
+      await signIn("google", { redirect: false, callbackUrl: "/" });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   const handleSignup = async (e: FormEvent) => {
-    setLoad(true)
-    e.preventDefault()
+    setLoad(true);
+    e.preventDefault();
     const body = {
-      name: firstName + ' ' + lastName,
+      name: firstName + " " + lastName,
       email,
       password,
-      phone
-    }
-    if (!firstName || !lastName || !email || !password || !confirmPassword || !phone) {
+      phone,
+    };
+
+    if (
+      !firstName ||
+      !lastName ||
+      !email ||
+      !password ||
+      !confirmPassword ||
+      !phone ||
+      !isTermsAgreed
+    ) {
       toast({
         className: cn(
-          'top-0 right-0 flex fixed md:max-w-[420px] md:top-4 md:right-4'
+          "top-0 right-0 flex fixed md:max-w-[420px] md:top-4 md:right-4"
         ),
-        title: 'Uh oh! Something went wrong.',
-        description: 'All field is required!!!',
-        variant: 'destructive',
-      })
-      setLoad(false)
-    } else {
-      try {
-        if (password.length < 8) {
-          toast({
-            className: cn(
-              'top-0 right-0 flex fixed md:max-w-[420px] md:top-4 md:right-4'
-            ),
-            title: 'Uh Oh! 😒',
-            description: 'The password at least 8 character!',
-            variant: 'destructive'
-          })
-          setLoad(false)
-        } else {
-          if (password === confirmPassword) {
-            setLoad(true)
-            await axios.post('/api/user/post', body)
-            toast({
-              className: cn(
-                'top-0 right-0 flex fixed md:max-w-[420px] md:top-4 md:right-4'
-              ),
-              title: 'Success!',
-              description: 'The user has been succesfully registered. You will redirect to login page!',
-              variant: 'default'
-            })
-            setTimeout(() => {
-              push('/user/login')
-              setLoad(false)
-            }, 1500)
-          } else {
-            toast({
-              className: cn(
-                'top-0 right-0 flex fixed md:max-w-[420px] md:top-4 md:right-4'
-              ),
-              title: 'Uh oh! Something went wrong.',
-              description: 'Please make the password and confirm password the same!',
-              variant: 'destructive',
-            })
-            setLoad(false)
-          }
-        }
-      } catch (error) {
-        setLoad(false)
-        console.log(error)
+        title: "Uh oh! Something went wrong.",
+        description: "All fields are required!",
+        variant: "destructive",
+      });
+      setLoad(false);
+      return;
+    }
+
+    try {
+      if (password.length < 8) {
         toast({
           className: cn(
-            'top-0 right-0 flex fixed md:max-w-[420px] md:top-4 md:right-4'
+            "top-0 right-0 flex fixed md:max-w-[420px] md:top-4 md:right-4"
           ),
-          title: 'Uh oh! Something went wrong.',
-          description: 'Error while fetching',
-          variant: 'destructive'
-        })
+          title: "Uh Oh! 😒",
+          description: "The password must be at least 8 characters!",
+          variant: "destructive",
+        });
+        setLoad(false);
+        return;
       }
+
+      if (password !== confirmPassword) {
+        toast({
+          className: cn(
+            "top-0 right-0 flex fixed md:max-w-[420px] md:top-4 md:right-4"
+          ),
+          title: "Uh oh! Something went wrong.",
+          description: "Password and confirm password do not match!",
+          variant: "destructive",
+        });
+        setLoad(false);
+        return;
+      }
+
+      setLoad(true);
+      await axios.post("/api/user/post", body);
+      toast({
+        className: cn(
+          "top-0 right-0 flex fixed md:max-w-[420px] md:top-4 md:right-4"
+        ),
+        title: "Success!",
+        description:
+          "The user has been successfully registered. You will be redirected to the login page!",
+        variant: "default",
+      });
+      setTimeout(() => {
+        push("/user/login");
+        setLoad(false);
+      }, 1500);
+    } catch (error) {
+      setLoad(false);
+      console.log(error);
+      toast({
+        className: cn(
+          "top-0 right-0 flex fixed md:max-w-[420px] md:top-4 md:right-4"
+        ),
+        title: "Uh oh! Something went wrong.",
+        description: "Error while fetching",
+        variant: "destructive",
+      });
     }
-  }
+  };
 
   useEffect(() => {
     if (phoneInputRef.current) {
-      phoneInputRef.current.value = dialCode
-      phoneInputRef.current.focus()
+      phoneInputRef.current.value = dialCode;
+      phoneInputRef.current.focus();
     }
-  }, [dialCode])
+  }, [dialCode]);
 
   return (
     <>
@@ -141,11 +158,23 @@ export default function Signup() {
               <div className="grid grid-cols-2 gap-4">
                 <div className="grid gap-2">
                   <Label htmlFor="first-name">First name</Label>
-                  <Input id="first-name" placeholder="Max" onChange={(e) => { setFirstName(e.target.value) }} />
+                  <Input
+                    id="first-name"
+                    placeholder="Max"
+                    onChange={(e) => {
+                      setFirstName(e.target.value);
+                    }}
+                  />
                 </div>
                 <div className="grid gap-2">
                   <Label htmlFor="last-name">Last name</Label>
-                  <Input id="last-name" placeholder="Robinson" onChange={(e) => { setLastName(e.target.value) }} />
+                  <Input
+                    id="last-name"
+                    placeholder="Robinson"
+                    onChange={(e) => {
+                      setLastName(e.target.value);
+                    }}
+                  />
                 </div>
               </div>
               <div className="grid gap-2">
@@ -154,39 +183,72 @@ export default function Signup() {
                   id="email"
                   type="email"
                   placeholder="dbix@example.com"
-                  onChange={(e) => { setEmail(e.target.value) }}
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
                 />
               </div>
               <div className="grid gap-2">
-                <Label htmlFor="email">Phone Number</Label>
+                <Label htmlFor="phone">Phone Number</Label>
                 <div className="flex items-center gap-1">
                   <Combobox setDialCode={setDialCode} />
                   <Input
-                    id="numb"
+                    id="phone"
                     type="tel"
-                    placeholder={`your phone number`}
-                    onChange={(e) => { setPhone(e.target.value) }}
+                    placeholder="Your phone number"
+                    onChange={(e) => {
+                      setPhone(e.target.value);
+                    }}
                     ref={phoneInputRef}
                   />
                 </div>
               </div>
               <div className="grid gap-2">
                 <Label htmlFor="password">Password</Label>
-                <Input id="password" type="password" onChange={(e) => { setPassword(e.target.value) }} />
+                <Input
+                  id="password"
+                  type="password"
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                />
               </div>
               <div className="grid gap-2">
-                <Label htmlFor="password">Confirm Password</Label>
-                <Input id="password" type="password" onChange={(e) => { setConfirmPassword(e.target.value) }} />
+                <Label htmlFor="confirm-password">Confirm Password</Label>
+                <Input
+                  id="confirm-password"
+                  type="password"
+                  onChange={(e) => {
+                    setConfirmPassword(e.target.value);
+                  }}
+                />
               </div>
-              <Button type="submit" disabled={load} className="w-full flex items-center gap-2">
+              <div className="flex items-center gap-2  w-fit">
+              <Checkbox/>
+              <Label htmlFor="terms" className="text-sm">
+                I agree to the terms and conditions
+              </Label>
+            </div>
+              <Button
+                type="submit"
+                disabled={load}
+                className="w-full flex items-center gap-2"
+              >
                 {load ? (
                   <>
                     <Shell size={24} strokeWidth={2} className="animate-spin" />
                     Loading...
                   </>
-                ) : 'Create an accoount!'}
+                ) : (
+                  "Create an account!"
+                )}
               </Button>
-              <Button variant="outline" type="button" className="w-full" onClick={handlesignupGoogle}>
+              <Button
+                variant="outline"
+                type="button"
+                className="w-full"
+                onClick={handlesignupGoogle}
+              >
                 Sign up with Google
               </Button>
             </form>
@@ -196,10 +258,10 @@ export default function Signup() {
                 Sign in
               </Link>
             </div>
+            
           </CardContent>
         </Card>
-
       </div>
     </>
-  )
+  );
 }
