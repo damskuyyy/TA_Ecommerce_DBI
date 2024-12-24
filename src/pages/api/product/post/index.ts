c8733168d2d6b0aee15d44863785dd4fe2682d7c
damskuyyy <muqoddam2711@gmail.com>
2024-12-24 15:08:43 +0700

    change prisma new

--- src/pages/api/product/post/index.ts
+++ src/pages/api/product/post/index.ts
@@ -21,7 +21,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
           sold,
           rate,
           stock,
-          minOrder
+          minOrder,
         }
       })
       res.status(200).send('data created succesfully!')
