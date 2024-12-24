2083907e73c9008d300c71ea145d24148527104c
damskuyyy <muqoddam2711@gmail.com>
2024-12-24 20:09:02 +0700

    change kecil code api product

--- src/pages/api/product/post/index.ts
+++ src/pages/api/product/post/index.ts
@@ -1,14 +1,33 @@
-import { NextApiRequest, NextApiResponse } from "next"
+import { NextApiRequest, NextApiResponse } from "next";
 import { randomUUID } from "crypto";
 import prisma from "@/utils/prisma";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { code_product, name, price, image, category, variants, details, spec, information, sold, rate, stock, minOrder, desc } = req.body
-  if (req.method === 'POST') {
+  const {
+    code_product,
+    name,
+    price,
+    desc,
+    image,
+    category,
+    variants,
+    details,
+    spec,
+    information,
+    sold,
+    rate,
+    stock,
+    minOrder,
+  } = req.body;
+  if (req.method === "POST") {
     try {
       await prisma.product.create({
         data: {
-          code_product: code_product ? code_product : randomUUID({ disableEntropyCache: true }).toUpperCase().slice(-12),
+          code_product: code_product
+            ? code_product
+            : randomUUID({ disableEntropyCache: true })
+                .toUpperCase()
+                .slice(-12),
           name,
           price,
           desc,
@@ -22,18 +41,16 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
           rate,
           stock,
           minOrder,
-        }
-      })
-      res.status(200).send('data created succesfully!')
-
+        },
+      });
+      res.status(200).send("data created succesfully!");
     } catch (error) {
-      res.status(500).json({ msg: 'Data Product Error!', error })
-      console.log(error)
+      res.status(500).json({ msg: "Data Product Error!", error });
+      console.log(error);
     }
   } else {
-    res.status(400).send('method not allowed!')
+    res.status(400).send("method not allowed!");
   }
+};
 
-}
-
-export default handler; 
+export default handler;
